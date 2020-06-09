9e5cc7309b2bd6cd4e6372c7fce390a9078618db
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-06-09 15:09:51 -0700

    aggregated task comments

--- models/intermediate/asana_task_comments.sql
+++ models/intermediate/asana_task_comments.sql
@@ -11,19 +11,40 @@ comments as (
     from story
     where comment_content is not null
 
+),
+
+user as (
+    
+    select *
+    from {{ ref('stg_asana_user') }}
+),
+
+join_user_comments as (
+
+    select
+    comments.target_task_id as task_id,
+    comments.created_at as commented_at,
+    comments.comment_content,
+    user.user_name
+
+    from
+    comments
+    left join user 
+        on user.user_id=comments.created_by_user_id 
+
+   --  order by task_id, comments.created_at asc
+
+),
+
+task_conversation as (
+
+    select
+        task_id,
+        string_agg(concat(cast(commented_at as string), '  -  ', user_name, ':  ', comment_content), '\n' order by commented_at asc) as conversation,
+        count(*) as number_of_comments
+
+    from join_user_comments        
+    group by 1
 )
--- ,
 
--- -- maybe call this something different...
--- agg_comments as (
-
---     select
---     target_task_id as task_id,
-
---     from
-
-)
-
-select * from comments
-
--- will probably delete this?
+select * from task_conversation
