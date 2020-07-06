865a5a5c7dbc185d3a13750d7ef24a3a1b2ff2c3
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-07-06 11:04:16 -0700

    cleaning up comments

--- models/asana_daily_metrics.sql
+++ models/asana_daily_metrics.sql
@@ -22,7 +22,7 @@ creations as (
     select 
         {{ dbt_utils.date_trunc('day', 'created_at') }} as day, 
         count(task_id) as number_of_tasks_created,
-        round(avg(days_open), 0) as avg_days_open, -- will be attached to the creation date, but this reflects the completion/current date
+        round(avg(days_open), 0) as avg_days_open, -- will be attached to the creation date, but this reflects the completion/time to current date
         round(avg(days_since_last_assignment), 0) as avg_days_assigned
 
     from task 
@@ -34,8 +34,6 @@ completions as (
     select 
         {{ dbt_utils.date_trunc('day', 'completed_at') }} as day, 
         count(task_id) as number_of_tasks_completed
-        -- round(avg(days_open), 0) as avg_close_time_days, 
-        -- round(avg(days_since_last_assignment), 0) as avg_assigned_close_time_days
 
     from task
     where is_completed
@@ -70,7 +68,6 @@ join_metrics as (
 
     select
         spine.date_day,
-        -- coalesce(assignments.day, creations.day, completions.day) as day,
         coalesce(number_of_tasks_created, 0) as number_of_tasks_created,
         coalesce(number_of_tasks_assigned, 0) as number_of_tasks_assigned,
         coalesce(number_of_tasks_completed, 0) as number_of_tasks_completed,
@@ -80,7 +77,7 @@ join_metrics as (
     from 
     spine
     left join creations on spine.date_day = cast(creations.day as datetime)
-    left join assignments on cast(assignments.day as datetime) = spine.date_day -- this or a union distinct of dates?
+    left join assignments on cast(assignments.day as datetime) = spine.date_day
     left join completions on cast(completions.day as datetime) = spine.date_day
 
 
