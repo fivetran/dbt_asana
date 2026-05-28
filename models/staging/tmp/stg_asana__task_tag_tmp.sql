74fd28b2c80a74a8fc31a5e8e43988de7cc7ad48
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-05-28 14:11:23 -0500

    update tmp models

--- models/staging/tmp/stg_asana__task_tag_tmp.sql
+++ models/staging/tmp/stg_asana__task_tag_tmp.sql
@@ -1,7 +1,7 @@
 {{ config(enabled=var('asana__using_task_tags', True)) }}
 
 {{
-    asana.asana_union_connections(
+    fivetran_utils.union_connections(
         connection_dictionary='asana_sources',
         single_source_name='asana',
         single_table_name='task_tag'
